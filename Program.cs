d3533e6283c937ac5ab1d3a12a61b3af1134d13c
sailakshmy <sailakshmy94@gmail.com>
2023-10-18 16:59:17 +0530

    Commit-4:- Configured a GraphQL client

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGraphQLServer().AddQueryType<Query>().AddType<BookType>();
 var app = builder.Build();
-
+app.UseStaticFiles();
 app.MapGet("/", () => "Hello World!");
 app.MapGraphQL();
 
